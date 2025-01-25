90c739283f15cd17cd434a44d5fe6a2af06dd2bb
percy <percy01729@gmail.com>
2025-01-25 21:53:52 +0530

    almost done with user backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../lib/db.js";
-dotenv.config();
 import authRoutes from "../routes/auth.route.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 const app = express();
-app.use("/api/auth",authRoutes);
-const PORT =process.env.PORT;
-app.listen(PORT,()=>{
-    console.log(`Server is running on PORT: ${PORT}`);
-    connectDB();
 
-})
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+  connectDB();
+});
