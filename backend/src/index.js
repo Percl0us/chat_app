fd49416ead75f795019d769fd4098be3f66a5532
percy <percy01729@gmail.com>
2025-01-26 13:44:30 +0530

    almost done with backend without socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../lib/db.js";
 import authRoutes from "../routes/auth.route.js";
+import messageRoute from "../routes/message.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -11,6 +12,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoute);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
